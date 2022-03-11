5d03e49c71d909204f23ec51f31032812df3705b
3DAlgoLab <83936830+3DAlgoLab@users.noreply.github.com>
2022-03-11 14:26:38 +0900

    .

--- blocking_client.py
+++ blocking_client.py
@@ -1,14 +1,29 @@
 import socket
+from multiprocessing.pool import ThreadPool
 
-sock = socket.socket()
-# host = socket.gethostname()
-host = '192.168.0.104'
-sock.connect((host, 12345))
 
-sock.setblocking(0)
+_pool = ThreadPool(2)
+_sock = socket.socket()
 
-# Or simply omit this line as by default TCP sockets
-# are in blocking mode
 
-data = b"Hello Python\n" * 10 * 1024 * 1024  # Huge amount of data to be sent
-assert sock.send(data)			        # Send data till true
+def send_proc(sock):
+    # host = socket.gethostname()
+    try:
+        host = '192.168.0.104'
+        sock.connect((host, 12345))
+
+        sock.setblocking(1)
+
+        # Or simply omit this line as by default TCP sockets
+        # are in blocking mode
+
+        data = b"Hello Python\n" * 10 * 1024 * 1024  # Huge amount of data to be sent
+        sock.send(data)			        # Send data till true
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    _pool.apply(send_proc, (_sock,))
+    input()
+    _sock.close()
