3f66da875591d4b0aa408deb606ff5554333eb5a
3DAlgoLab <83936830+3DAlgoLab@users.noreply.github.com>
2022-03-11 14:11:16 +0900

    blocking socket test

---
+++ blocking_server.py
@@ -0,0 +1,23 @@
+
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+port = 12345
+
+print(host, port)
+
+s.bind((host, port))
+s.listen(5)
+
+while True:
+    conn, addr = s.accept()		# accept the connection
+
+    data = conn.recv(1024)
+    while data:			        # till data is coming
+        print(data)
+        data = conn.recv(1024)
+    print("All Data Received")  # Will execute when all data is received
+    conn.close()
+    break
